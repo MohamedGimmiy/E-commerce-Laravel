381282d11df97c191bc1f7f4eb4de62ec0fe3f4c
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-10-24 18:18:49 +0200

    creating registeration page

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -1,4 +1,32 @@
 @extends('layouts.master')
+@section('title','Register')
 @section('content')
-    Register page
+    <section class="login-page">
+        <div class="login-form-box">
+            <div class="login-title">Register</div>
+            <div class="login-form">
+                <form action="" method="post">
+                    <div class="field">
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name" placeholder="John Doe">
+                    </div>
+                    <div class="field">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="John@gmail.com">
+                    </div>
+                    <div class="field">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="********">
+                    </div>
+                    <div class="field">
+                        <label for="password_confirmation">Confirm password</label>
+                        <input type="password" name="password_confirmation" id="name" placeholder="********">
+                    </div>
+                    <div class="field">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
 @endsection
