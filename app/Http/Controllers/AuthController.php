7f16b54b85d90048a2dda81b5cf03a4a5d102966
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-10-24 19:18:25 +0200

    register & logout functionalities

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -17,9 +21,24 @@ class AuthController extends Controller
         return view('pages.register');
     }
     // register user
-
+    public function postRegister(RegisterRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        Auth::login($user);
+        return back()->with('success', 'Successfully Logged in');
+    }
     // login user
 
     // reset password
 
+    //logout
+    public function logout()
+    {
+        Auth::logout();
+        return back();
+    }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <main class="homepage">
         @include('pages.components.home.header')
+        <form action="{{route("logout")}}" method="post">
+            @csrf
+            <button class="btn btn-primary">logout</button>
+        </form>
     </main>
     Home page section
 @endsection
